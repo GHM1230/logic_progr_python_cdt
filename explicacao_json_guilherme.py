d47db8b13855de5c23fbb01049799404736c73ad
Gustavo Rocha <>
2026-04-15 15:42:29 -0300

    Update explicacao_json_guilherme.py

--- explicacao_json_guilherme.py
+++ explicacao_json_guilherme.py
@@ -1,14 +1,14 @@
 import os
 import json
 
-def configurar_sistema()
+def configurar_sistema():
     if not os.path.exists("uploads_projetos"):
         os.makedirs("uploads_projetos")
 
 def listar_projetos():
     arquivos = [f for f in os.listdir("uploads_projetos") if f.endswith(".json")]
     print('\n'+ '~' * 40)
-    print('projetos cadastrados:')
+    print('   PROJETOS CADASTRADOS:')
     print('~' * 40)
 
     if not arquivos:
@@ -51,4 +51,48 @@ def gerenciar_projeto():
         with open(caminho, "w", encoding="utf-8") as f:
             json.dump(dados, f, ensure_ascii=False, indent=4)
         print("Projeto atualizado com sucesso!")
-    except 
+
+    else:
+        print("[ERRO] Escolha inválida. voltando ao menu.")
+
+def fazer_upload_json():
+
+    print('\n'+ '~' * 40)
+    print('   NOVO UPLOAD DE PROJETO')
+    print('~' * 40)
+    nome_aluno = input("Nome do aluno: ").strip()
+    resumo = input("Resumo do projeto: ")
+
+    dados = {"aluno": nome_aluno, "projeto": resumo}
+
+    nome_fich = f"projeto_{nome_aluno.replace(' ', '_')}.json"
+
+    caminho = f"uploads_projetos/projetos_{nome_fich}.json"
+
+    with open(caminho, "w", encoding="utf-8") as f:
+        json.dump(dados, f, ensure_ascii=False, indent=4)
+    print(f"\n[SUCESSO] Projeto de {nome_aluno} guardado em {caminho}!")
+
+def menu():
+    configurar_sistema()
+    while True:
+        print('\n'+ '~' * 40)
+        print('   SISTEMA DE ARQUIVOS JOVENS 3.0')
+        print('~' * 40)
+        print("1. Inserir novo projeto")
+        print("2. Listar e Alterar projetos")
+        print("3. Sair")
+
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == '1':
+            fazer_upload_json()
+        elif escolha == '2':
+            gerenciar_projeto()
+        elif escolha == '3':
+            print("Encerrando... Até logo!")
+            break
+        else:
+            print("\n[ERRO] Opção inválida!")
+
+menu()
